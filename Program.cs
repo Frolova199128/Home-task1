f27e7599997f225f49668692e15eb6628bd02948
Frolova199128 <125869363+Frolova199128@users.noreply.github.com>
2023-04-06 21:05:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 2
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 == num2)
+// {
+//    Console.WriteLine("Числа равны");
+//}
+//if(num1 > num2)
+//{
+//    Console.WriteLine(num1);
+//}
+// else
+//{
+//    Console.WriteLine (num2);
+// }
+
+// Задача 4
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2 && num1 > num3)
+// {
+//     Console.Write(num1);
+// }
+// if(num2 > num1 && num2 > num3)
+// {
+//     Console.Write(num2);
+// }
+// if(num3 > num1 && num3 > num2)
+// {
+//     Console.Write(num3);
+// }
+// if(num1 == num2 && num1 > num3)
+// {
+//     Console.Write(num1);
+// }
+// if(num1 == num3 && num1 > num2)
+// {
+//     Console.Write(num1);
+// }
+// if(num2 == num3 && num2 > num1)
+// {
+//     Console.Write(num2);
+// }    
+// if(num1 == num2 && num1 == num3)
+// {
+//     Console.Write(num1);
+// }  
+
+// Задача 6
+// Console.Write("Введите число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if(num1%2==0)
+// {
+//     Console.Write("Yes");
+// }
+// else
+// {
+//     Console.Write("No");
+// }
+
+// Задача 8 
+// Console.Write("Введите положительное число ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int current = 1; current <= N; current++) 
+
+// if(current%2 == 0)
+//    Console.Write(current);
